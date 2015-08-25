310c8460f012978ed3243bf3be58206411fe2dc1
K.Misaki <sabuaca1o4@gmail.com>
2015-08-25 14:03:00 +0900

    use JSONAnalyze libs

--- app/src/main/java/jp/mickey305/authtodosample/http/AuthDocomoJSON.java
+++ app/src/main/java/jp/mickey305/authtodosample/http/AuthDocomoJSON.java
@@ -1,12 +1,12 @@
 package jp.mickey305.authtodosample.http;
 
+import com.mickey305.androidCommons.analysis.json.JSONPicker;
+import com.mickey305.androidCommons.analysis.json.JSONToken;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
-
-import jp.mickey305.authtodosample.util.JSONPicker;
 
 public class AuthDocomoJSON {
     public static final String TAG = AuthDocomoJSON.class.getSimpleName();
@@ -18,38 +18,35 @@ public class AuthDocomoJSON {
     }
 
     private LinkedList<String> getKeys() { return keys; }
+
     private JSONObject getJsonObject() { return jsonObject; }
 
     public void setJsonObject(JSONObject jsonObject) { this.jsonObject = jsonObject; }
-    public void addKey(String key) { getKeys().add(key); }
-    public void clearKey() { getKeys().clear(); }
-    public String removeKey() { return getKeys().remove(); }
-    public ArrayList<String> getValues() throws JSONException {
-        ArrayList<String> _tmp;
-        final JSONPicker _picker = new JSONPicker(getJsonObject());
-        _tmp = _picker.getValues(getKeys());
-        return _tmp;
-    }
-    public boolean isExistAllKeys() {
-        final JSONPicker _picker = new JSONPicker(getJsonObject());
-        return _picker.isExistAllKeys(getKeys());
-    }
 
+    public void addKey(String key) { getKeys().add(key); }
+
+    public void clearKey() { getKeys().clear(); }
+
+    public String removeKey() { return getKeys().remove(); }
+
+    @SuppressWarnings("unchecked")
     public String getValue(String key, int index) throws JSONException {
         String _value;
-        ArrayList<String> _list;
+        LinkedList<JSONToken> _list;
         final JSONPicker _picker = new JSONPicker(getJsonObject());
 
         _list = _picker.getValues(key);
 
         try {
-            _value = _list.get(index);
+            _value = _list.get(index).getValue().to_s();
         } catch (IndexOutOfBoundsException e) {
             throw(new JSONException(TAG+": IndexOutOfBoundsException"));
         }
 
         return _value;
     }
+
+    @SuppressWarnings("unchecked")
     public boolean isExistKey(String key) {
         return new JSONPicker(getJsonObject()).isExistKey(key);
     }
